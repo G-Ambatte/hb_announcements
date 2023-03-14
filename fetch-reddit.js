b589b2d820cb1fffba3a6d5e2c88bdbe9cf83cd5
G.Ambatte <sean@robertson-family.nz>
2023-03-14 20:27:31 +1300

    Add headings for title and date

--- fetch-reddit.js
+++ fetch-reddit.js
@@ -4,7 +4,7 @@ const util = require('util');
 request('https://raw.githack.com/naturalcrit/homebrewery/master/changelog.md', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     let data = body.split('\n### ')[1];
-    data = '**Change Log**\r\nFor a full record of development, visit our Github Page - https://github.com/naturalcrit/homebrewery\r\n\r\n' + data + '---\r\n\r\nAs always, if you find any issues or have a suggestion, please feel free to let us know!\r\n\r\n\\- G';
+    data = '## Change Log\r\nFor a full record of development, visit our Github Page - https://github.com/naturalcrit/homebrewery\r\n\r\n' + data + '---\r\n\r\nAs always, if you find any issues or have a suggestion, please feel free to let us know!\r\n\r\n\\- G';
 
     const replacements = [
       { search: '{{taskList', replace: '' },
@@ -17,6 +17,11 @@ request('https://raw.githack.com/naturalcrit/homebrewery/master/changelog.md', f
     replacements.forEach((terms)=>{
       data = data.split(terms.search).join(terms.replace);
     });
+
+    let lines = data.split('\r\n');
+    lines[3] = '### ' + lines[3];
+
+    data = lines.join('\r\n');
 
     require('child_process').spawn('clip').stdin.end(data);
     console.log(data);
